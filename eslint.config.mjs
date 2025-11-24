78f08e8d4ff0b5de18e112cce18eae265300feaa
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-11-24 23:44:29 +0900

    chore(#1): eslint 설치 및 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,44 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  {
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      import: require("eslint-plugin-import"),
+      prettier: require("eslint-plugin-prettier"),
+    },
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts"
+    ],
+    extends: [
+      "plugin:@typescript-eslint/recommended",
+      "plugin:import/recommended",
+      "plugin:prettier/recommended",
+    ],
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          "newlines-between": "always",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "prettier/prettier": "error",
+    },
+  },
 ]);
-
-export default eslintConfig;
