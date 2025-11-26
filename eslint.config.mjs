a6be2ee44e15693d46eab8a577de890f670c4e23
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-11-26 12:29:15 +0900

    chore(#1): 커밋 전 eslint 검사

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,45 +1,42 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig } from 'eslint/config'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import importPlugin from 'eslint-plugin-import'
+import prettierPlugin from 'eslint-plugin-prettier'
 
-export default defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  {
-    plugins: {
-      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
-      import: require("eslint-plugin-import"),
-      prettier: require("eslint-plugin-prettier"),
-    },
-    languageOptions: {
-      parser: require("@typescript-eslint/parser"),
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-    },
-    ignores: [
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts"
-    ],
-    extends: [
-      "plugin:@typescript-eslint/recommended",
-      "plugin:import/recommended",
-      "plugin:prettier/recommended",
-    ],
-    rules: {
-      "import/order": [
-        "error",
-        {
-          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
-          "newlines-between": "always",
-        },
-      ],
-      "@typescript-eslint/no-unused-vars": ["warn"],
-      "no-console": ["warn", { allow: ["warn", "error"] }],
-      "prettier/prettier": "error",
+export default defineConfig({
+  plugins: {
+    '@typescript-eslint': tseslint,
+    import: importPlugin,
+    prettier: prettierPlugin,
+  },
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
   },
-]);
+  ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  rules: {
+    // TypeScript ESLint rules
+    ...tseslint.configs.recommended.rules,
+
+    // import plugin rules
+    ...importPlugin.configs.recommended.rules,
+
+    // prettier rules
+    ...prettierPlugin.configs.recommended.rules,
+
+    // 커스텀 rules
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+})
