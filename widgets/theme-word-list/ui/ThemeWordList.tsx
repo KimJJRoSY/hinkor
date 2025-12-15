ea6520515ad4cbc3a77c48432dc77475fda0ebe3
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-12-15 21:55:35 +0900

    fix(#16): ThemeWordList - 널 타입 체크 및 의존성 추가

--- widgets/theme-word-list/ui/ThemeWordList.tsx
+++ widgets/theme-word-list/ui/ThemeWordList.tsx
@@ -2,11 +2,12 @@
 
 import { Word } from '@/entities/word'
 import { supabase } from '@/shared/api/supabase/client'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { BookOpenCheck, Check, EyeClosed, Eye } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { MoveToButton } from '@/shared/ui'
 import { WordItem } from '@/features/word-list'
+import useToast from '@/shared/utils/useToast'
 
 interface Props {
   params: string
@@ -16,15 +17,17 @@ function ThemeWordList({ params }: Props) {
   const [wordList, setData] = useState<Word[]>()
   const [opposition, setOpposition] = useState<string>()
   const [isHidden, setIsHidden] = useState(false)
+  const { error } = useToast()
 
-  const getWordList = async () => {
+  const getWordList = useCallback(async () => {
     const { data } = await supabase.from('theme_list').select('*').eq('theme', params)
-    if (!data) return
+    if (!data) return error('테마 단어 데이터가 없습니다.')
     setData(data[0].words)
     setOpposition(data[0].opposition)
-  }
+  }, [params, error])
 
   const gotoCheckOpposition = () => {
+    if (!opposition) return error('반의어 데이터가 없습니다.')
     router.push(`/theme?id=${opposition}`)
   }
 
