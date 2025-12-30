18e37bde45d97cbeffbeee61b8ae21f57711a7c6
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-12-31 00:21:58 +0900

    refactor(#40): 네비게이션에서 isActive 조건 추가

--- shared/ui/NavigationBar.tsx
+++ shared/ui/NavigationBar.tsx
@@ -1,15 +1,22 @@
 'use client'
 
 import NavigationButton from './NavigationButton'
-import { usePathname } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation'
 
 export default function NavigationBar() {
   const path = usePathname()
+  const params = useSearchParams()
 
   return (
     <div className="flex gap-2">
-      <NavigationButton label={'DAY'} isActive={path.startsWith('/day') || path === '/'} />
-      <NavigationButton label={'THEME'} isActive={path.startsWith('/theme')} />
+      <NavigationButton
+        label={'DAY'}
+        isActive={path.startsWith('/day') || path === '/' || params.get('label') === 'day'}
+      />
+      <NavigationButton
+        label={'THEME'}
+        isActive={path.startsWith('/theme') || params.get('label') === 'theme'}
+      />
     </div>
   )
 }
